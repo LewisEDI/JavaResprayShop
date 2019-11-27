2f4d8b1775d7cd7656eddb9da709bb3a44da3a46
Lewis Bogle <bogfly2000@hotmail.com>
2019-11-27 15:25:43 +0000

    tests LawAbidingCitizen getsDoublePrice for creditcard

--- src/test/java/StolenCarTest.java
+++ src/test/java/StolenCarTest.java
@@ -2,6 +2,7 @@ import cars.StolenCar;
 import org.junit.Before;
 import org.junit.Test;
 import people.Criminal;
+import people.LawAbidingCitizen;
 
 import static org.junit.Assert.assertEquals;
 
@@ -9,11 +10,13 @@ public class StolenCarTest {
 
     StolenCar stolenCar;
     Criminal criminal;
+    LawAbidingCitizen lawAbidingCitizen;
 
     @Before
     public void before(){
         stolenCar = new StolenCar("blue", "Cyber Truck", 2019, true);
         criminal = new Criminal("Raf", "Cat swinging", "cash", 10000);
+        lawAbidingCitizen = new LawAbidingCitizen("Laurie", 300, "Full Stack Developer", "Credit Card");
     }
 
     @Test
@@ -24,5 +27,10 @@ public class StolenCarTest {
     @Test
     public void getDoublePrice(){
         assertEquals(400, stolenCar.priceFor(criminal), 0.01);
+    }
+
+    @Test
+    public void getDoublePrice__for_creditcard(){
+        assertEquals(200, stolenCar.priceFor(lawAbidingCitizen), 0.01);
     }
 }
